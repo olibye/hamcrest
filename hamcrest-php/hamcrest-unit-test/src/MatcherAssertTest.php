c983924f172cdae90ffe89fc50178dde935bf231
sebastian.bergmann <sebastian.bergmann@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2007-12-29 09:54:38 +0000

    - Start work on hamcrest-unit-test.

---
+++ hamcrest-php/hamcrest-unit-test/src/MatcherAssertTest.php
@@ -0,0 +1,21 @@
+<?php
+/*  Copyright (c) 2000-2008 hamcrest.org
+ */
+namespace Hamcrest;
+
+require_once 'PHPUnit/Framework.php';
+require_once 'Hamcrest.php';
+
+class MatcherAssertTest extends ::PHPUnit_Framework_TestCase {
+    public function testIncludesDescriptionOfTestedValueInErrorMessage() {
+        $this->markTestIncomplete();
+    }
+
+    public function testDescriptionCanBeElided() {
+        $this->markTestIncomplete();
+    }
+    
+    public function testCanTestBooleanDirectly() {
+        $this->markTestIncomplete();
+    }
+}

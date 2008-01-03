daca982ff709304cda7bd05be8f32237faef4699
sebastian.bergmann <sebastian.bergmann@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2008-01-03 06:22:49 +0000

    - Optionally alias namespaced factory functions in global scope. # PHP does not support importing functions from a namespace.

--- hamcrest-php/Hamcrest.php
+++ hamcrest-php/Hamcrest.php
@@ -20,3 +20,49 @@ require_once 'Hamcrest/Matcher/IsNot.php';
 require_once 'Hamcrest/Matcher/IsNull.php';
 require_once 'Hamcrest/Matcher/Is.php';
 require_once 'Hamcrest/Matcher/IsSame.php';
+
+if (!defined('HAMCREST_DO_NOT_ALIAS_FUNCTIONS_IN_GLOBAL_SCOPE')) {
+    function allOf($matchers) {
+        return Hamcrest::allOf($matchers);
+    }
+
+    function anyOf($matchers) {
+        return Hamcrest::anyOf($matchers);
+    }
+
+    function describedAs($description, Matcher $matcher, array $values) {
+        return Hamcrest::describedAs($description, $matcher, $values);
+    }
+
+    function is(Matcher $matcher) {
+        return Hamcrest::is($matcher);
+    }
+
+    function anything($description = 'ANYTHING') {
+        return Hamcrest::anything($description);
+    }
+
+    function closeTo($operand, $error) {
+        return Hamcrest::closeTo($operand, $error);
+    }
+
+    function equalTo($operand) {
+        return Hamcrest::equalTo($operand);
+    }
+
+    function anInstanceOf($type) {
+        return Hamcrest::anInstanceOf($type);
+    }
+
+    function not($matcherOrValue) {
+        return Hamcrest::not($matcherOrValue);
+    }
+
+    function notNullValue() {
+        return Hamcrest::notNullValue();
+    }
+
+    function sameInstance($object) {
+        return Hamcrest::sameInstance($object);
+    }
+}

4543e9055453f06d4c4123116406f16cfc2b2637
sebastian.bergmann <sebastian.bergmann@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2008-01-26 09:07:05 +0000

    - Extend PHPUnit_Framework_AssertionFailedError when available.

--- hamcrest-php/Hamcrest/AssertionError.php
+++ hamcrest-php/Hamcrest/AssertionError.php
@@ -3,5 +3,23 @@
  */
 namespace Hamcrest;
 
-class AssertionError extends RuntimeException {
+$paths = explode(PATH_SEPARATOR, get_include_path());
+
+foreach ($paths as $path) {
+    $fullpath = $path . DIRECTORY_SEPARATOR . 'PHPUnit/Framework.php';
+
+    if (file_exists($fullpath)) {
+        $file = realpath($fullpath);
+        break;
+    }
+}
+
+if (isset($file)) {
+    require_once 'PHPUnit/Framework.php';
+
+    class AssertionError extends ::PHPUnit_Framework_AssertionFailedError {
+    }
+} else {
+    class AssertionError extends RuntimeException {
+    }
 }

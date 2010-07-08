4d8c71875d5f81e732ed16e0185637ac4c6ab6dd
dharkness <dharkness@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2010-07-08 20:34:47 +0000

    Fixed parameter type in doc comment.

--- hamcrest-php/hamcrest/Hamcrest/SelfDescribing.php
+++ hamcrest-php/hamcrest/Hamcrest/SelfDescribing.php
@@ -11,15 +11,15 @@ require_once 'Hamcrest/Description.php';
  */
 interface Hamcrest_SelfDescribing
 {
-  
+
   /**
-   * Generates a description of the object.  The description may be part of a
-   * a description of a larger object of which this is just a component, so it 
-   * should be worded appropriately.
-   * 
-   * @param HamcrestDescription $description
+   * Generates a description of the object.  The description may be part
+   * of a description of a larger object of which this is just a component,
+   * so it should be worded appropriately.
+   *
+   * @param Hamcrest_Description $description
    *   The description to be built or appended to.
    */
   public function describeTo(Hamcrest_Description $description);
-  
+
 }
